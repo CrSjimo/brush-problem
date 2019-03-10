43e2ee087f8b487d61209bbac9016f1757e4fa5e
CrSjimo <crs_16423@outlook.com>
2019-03-10 12:09:00 +0800

    aa

--- backpack_C.c
+++ backpack_C.c
@@ -2,7 +2,7 @@
 #include<string.h>
 int dp[1010][1010],c[1010];
 int max(int a,int b){
-    return a>b?a:b;
+    return (a>b)?a:b;
 }
 int main(){
     int n;
@@ -14,15 +14,15 @@ int main(){
         scanf("%d",&money);
         for(int i=1;i<=n;i++){
             for(int j=0;j<money-5;j++){
-                if(j>=c[i]){
+                // if(j>=c[i]){
+                    // printf("%d %d",dp[i-1][j-c[i]]+c[i],dp[i-1][j]);
                     dp[i][j]=max(dp[i-1][j-c[i]]+c[i],dp[i-1][j]);
-                }else{
-                    dp[i][j]=dp[i-1][j];
-                }
-                
+                    // printf(" %d\n",dp[i][j]);
+                    // printf("%d %d\n",i,j);
+                // }    
             }
         }
-        printf("%d",dp[n][money-5]);
+        printf("%d",money-dp[n][money-5-1]);
     }
-    
+    return 0;
 }
