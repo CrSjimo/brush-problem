18f99def081f11548a5e4eaf17fcb6d9bad61967
CrSjimo <crs_16423@outlook.com>
2019-05-12 11:55:01 +0800

    add bugs

---
+++ bfs1.cpp
@@ -0,0 +1,42 @@
+#include<cstdio>
+#include<cstring>
+#include<queue>
+int n,a,b;
+int num[220];
+bool visited[220];
+struct Node{
+    int id;
+    int dist;
+};
+typedef std::queue<Node> Queue;
+void addNewNode(Queue &q,int nodeId,int nodeDist){
+    if(nodeId <= n && nodeId >0 && !visited[nodeId]){
+        Node tmp;
+        tmp.id = nodeId;
+        tmp.dist = nodeDist;
+        q.push(tmp);
+        visited[nodeId] = 1;
+    }
+}
+int bfs(int* num,int startPos,int endPos){
+    Queue q;
+    memset(visited,0,sizeof(visited));
+    Node tmp;
+    tmp.id = startPos;
+    tmp.dist = 0;
+    visited[startPos] = 1;
+    while(!q.empty()){
+        
+    }
+    return -1;
+}
+int main(){
+    while(std::scanf("%d",&n) && n){
+        std::scanf("%d%d",&a,&b);
+        for(int i = 1;i<=n;i++){
+            std::scanf("%d",num+i);
+        }
+        std::printf("%d\n",bfs(num,a,b));
+    }
+    return 0;
+}
