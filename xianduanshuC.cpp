8b7ac4cb193ef3386d7e3f8b2e57684b0e719982
crindzebra <crs_16423@outlook.com>
2019-07-13 17:59:04 +0800

    original

--- xianduanshuC.cpp
+++ xianduanshuC.cpp
@@ -51,7 +51,83 @@ void pushDown(int o,int len){
         }
     }
 }
+int ql,qr;
+int query(int o,int l,int r){
+    if(ql<=l&&r<=qr){
+        int ans = 0;
+        four(i){
+            ans += sumv[i][o]*(1<<i);
+        }
+        return ans;
+    }
+    int m = (l+r)/2,len = r-l+1;
+    pushDown(o,len);
+    int ans = 0;
+    if(ql<=m){
+        ans+=query(o*2,l,m);
+    }
+    if(qr>m){
+        ans+=query(o*2+1,m+1,r);
+    }
+    return ans;
+}
+int v;
+char ops[10];
+char& op = *ops;
+void update(int o,int l,int r){
+    int len = r-l+1;
+    if(ql<=l&&r<=qr){
+        four(i){
+            int is1 = v&(1<<i);
+            if(op == 'O' && is1){
+                sumv[i][o] = len;
+                opv[i][o] = 1;
+                xorv[i][o] = 0;
+            }else if(op == 'X' && is1){
+                sumv[i][o] = len - sumv[i][o];
+                doXOR(o,i);
+            }else if(op == 'A' && !is1){
+                sumv[i][o] = 0;
+                opv[i][o] = 0;
+                xorv[i][o] = 0;
+            }
+            
+        }
+    }else{
+        pushDown(o,len);
+        int m = (l+r)/2,lc = o*2,rc = o*2+1;
+        if(ql<=m){
+            update(lc,l,m);
+        }
+        if(qr>m){
+            update(rc,m+1,r);
+        }
+        pushUp(o);
+    }
+}
 int main(){
-    
+    int cas;
+    scanf("%d",&cas);
+    while(cas--){
+        int n,m;
+        scanf("%d%d",&n,&m);
+        for(int i = 1;i<=n;i++){
+            scanf("%d",num+i);
+        }
+        build(1,1,n);
+        while(m--){
+            scanf("%s",ops);
+            // printf("%c\n",ops[0]);
+            if(op == 'S'){
+                scanf("%d%d",&ql,&qr);
+                ql++;qr++;
+                printf("%d\n",query(1,1,n));
+            }else{
+                scanf("%d%d%d",&v,&ql,&qr);
+                ql++;qr++;
+                update(1,1,n);
+            }
+        }
+    }
     return 0;
 }
